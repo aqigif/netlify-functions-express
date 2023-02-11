c7a9ac936479d076e860afd19b69d573085e329a
aqigif <aqilgifari0@gmail.com>
2023-02-12 04:09:15 +0700

    fix: maps try catch

--- functions/app/index.js
+++ functions/app/index.js
@@ -76,19 +76,19 @@ export default function expressApp(functionName) {
     })
   })
 
-  router.get('/maps/', function(req, res) {
+  router.get('/maps/', async function(req, res) {
     const place_id = req.params.place_id;
     const apiKey = req.params.key;
     const apiUrl = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&key=${apiKey}`;
 
-    axios.get(apiUrl)
-      .then((response) => {
-        res.json(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send('An error occurred while fetching data from the Google Maps API');
-      });
+    try {
+      const resp = await axios.get(apiUrl)
+      if (resp.status === 200) {
+        res.json(resp.data);
+      } else throw resp
+    } catch (error) {
+      res.status(500).send('An error occurred while fetching data from the Google Maps API');
+    }
   });
 
   // app.get('/maps/photo/:maxwidth/:photoreference', (req, res) => {
