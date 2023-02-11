5a45b694a1318970fd7b0c21daf9b8485f083fd4
aqigif <aqilgifari0@gmail.com>
2023-02-12 03:46:24 +0700

    feat: add  maps functions

--- functions/app/index.js
+++ functions/app/index.js
@@ -5,6 +5,10 @@ import morgan from 'morgan'
 import bodyParser from 'body-parser'
 import compression from 'compression'
 import customLogger from '../utils/logger'
+const axios = require('axios');
+
+require('dotenv').config();
+const apiKey = process.env.API_KEY;
 
 /* My express App */
 export default function expressApp(functionName) {
@@ -82,6 +86,37 @@ export default function expressApp(functionName) {
   router.get('/hello/', function(req, res) {
     res.send('hello world')
   })
+
+
+app.get('/maps/:place_id', (req, res) => {
+  const place_id = req.params.place_id;
+  const apiUrl = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&key=${apiKey}`;
+
+  axios.get(apiUrl)
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('An error occurred while fetching data from the Google Maps API');
+    });
+});
+
+app.get('/maps/photo/:maxwidth/:photoreference', (req, res) => {
+  const photoreference = req.params.photoreference;
+  const maxwidth = req.params.maxwidth;
+  const apiUrl = `https://maps.googleapis.com/maps/api/place/photo?photoreference=${photoreference}&maxwidth=${maxwidth}&key=${apiKey}`;
+
+  axios.get(apiUrl, { responseType: 'arraybuffer' })
+    .then((response) => {
+      res.set('Content-Type', 'image/jpeg');
+      res.send(Buffer.from(response.data, 'binary'));
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('An error occurred while fetching data from the Google Maps API');
+    });
+});
 
   // Attach logger
   app.use(morgan(customLogger))
